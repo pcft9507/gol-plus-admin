354d4312a988e522390be5b9bea006662abdefc3
pcft9507 <31539142+pcft9507@users.noreply.github.com>
2022-08-25 08:52:25 +0900

    손해사정사 상담상세

--- build/assets/js/ui.js
+++ build/assets/js/ui.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function () {
+  // 인풋 라인 활성화
+  $('.effect-1').on('blur', function (){
+    var valueLength = $(this).val().length;
+    if (valueLength > 0) {
+      $(this).addClass('is-active');
+    } else {
+      $(this).removeClass('is-active');
+    }
+  });
+})
