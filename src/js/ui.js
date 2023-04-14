0c4d2da4f9f718b6fcb9d4c2ec06fad5b1d24dde
pcft9507 <31539142+pcft9507@users.noreply.github.com>
2023-04-14 10:27:09 +0900

    테이블 수정

--- src/js/ui.js
+++ src/js/ui.js
@@ -24,3 +24,18 @@ $.fn.inpLabel = function () {
     inpArray[i].after('<label for="' + inpId[i] + '" class="inp-label">' + inpPlaceholder[i] + '</label>');
   });
 }
+
+// 추천 상담 리스트 모바일에서 지정된 칼럼만 나오게 하기
+function reponsiveTableColumn(colIdx) {
+  var dataTbl = $('#dataTable');
+  dataTbl.addClass('reponsive-table');
+  var inpIdx = [];
+  inpIdx = colIdx;
+  var dataTblRowLength = dataTbl.find('tbody tr').length;
+  inpIdx.forEach(function (i) {
+    $('#dataTable thead th').eq(i).addClass('mo-view');
+    for (var j = 0; j < dataTblRowLength;j++) {
+      $('#dataTable tbody tr').eq(j).find('td').eq(i).addClass('mo-view');
+    }
+  });
+}
